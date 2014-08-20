b6c9fdcdbc62cad0569eb5baea5eeb3e0c1e20a7
lclaudon <lclaudon@20409334-78a3-4d4d-9d24-00d80d4599ef>
2014-08-20 08:58:02 +0000

    STDSASSCL-928
    git-svn-id: https://psdsvn.sicpa-net.ads/standard-framework/standard-sas-scl-modules/standard-sas-scl-parent/trunk@46945 20409334-78a3-4d4d-9d24-00d80d4599ef

--- standard-sas-scl-spring-scl/src/main/java/com/sicpa/standard/sasscl/ioc/SpringConfigSCL.java
+++ standard-sas-scl-spring-scl/src/main/java/com/sicpa/standard/sasscl/ioc/SpringConfigSCL.java
@@ -7,7 +7,8 @@ public class SpringConfigSCL extends SpringConfig {
 	public static final String PRINTER = PREFIX + "printer";
     public static final String PRINTER_DOMINO = PREFIX + "printer-domino";
     public static final String PRINTER_LEIBINGER = PREFIX + "printer-leibinger";
-	public static final String CODING = PREFIX + "coding";
+    public static final String PRINTER_OI2JET = PREFIX + "printer-oi2jet";
+    public static final String CODING = PREFIX + "coding";
 	public static final String POST_PACKAGE = PREFIX + "postPackage";
 	public static final String SCHEDULER_SCL_ADDITIONAL_TASKS = PREFIX + "schedulerSCL";
 
@@ -16,7 +17,10 @@ public class SpringConfigSCL extends SpringConfig {
 		this.config.put(PRINTER, "spring" + File.separator + "printer.xml");
         config.put(PRINTER_DOMINO, "spring/printer-domino.xml");
         config.put(PRINTER_LEIBINGER, "spring/printer-leibinger.xml");
+//        Oi2Jet not immplemented yet
+//        config.put(PRINTER_OI2JET, "spring/printer-oi2jet.xml");
 
-		this.config.put(SCHEDULER_SCL_ADDITIONAL_TASKS, "spring" + File.separator + "schedulerSCL.xml");
+
+        this.config.put(SCHEDULER_SCL_ADDITIONAL_TASKS, "spring" + File.separator + "schedulerSCL.xml");
 	}
 }
