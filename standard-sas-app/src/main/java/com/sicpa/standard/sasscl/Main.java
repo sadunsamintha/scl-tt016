4d3f9591b8b38f0bd2b68cf01ad17b783b9b578e
lclaudon <lclaudon@20409334-78a3-4d4d-9d24-00d80d4599ef>
2014-09-09 13:25:41 +0000

    STDSASSCL-938
    git-svn-id: https://psdsvn.sicpa-net.ads/standard-framework/standard-sas-scl-modules/standard-sas-scl-parent/trunk@47390 20409334-78a3-4d4d-9d24-00d80d4599ef

--- standard-sas-app/src/main/java/com/sicpa/standard/sasscl/Main.java
+++ standard-sas-app/src/main/java/com/sicpa/standard/sasscl/Main.java
@@ -1,15 +1,18 @@
 package com.sicpa.standard.sasscl;
 
-import java.io.File;
-import java.io.InputStream;
-
+import com.sicpa.standard.client.common.launcher.LoaderConfig;
+import com.sicpa.standard.gui.plaf.SicpaLookAndFeel;
+import com.sicpa.standard.sasscl.ioc.SpringConfig;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.sicpa.standard.client.common.launcher.LoaderConfig;
-import com.sicpa.standard.gui.plaf.SicpaLookAndFeel;
-import com.sicpa.standard.sasscl.ioc.SpringConfig;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
 
 public class Main {
 
@@ -33,45 +36,62 @@ public class Main {
 		SpringConfig config = new SpringConfig();
 		// config.put(SpringConfig.OFFLINE_COUNTING, "spring" + File.separator + "offlineCounting.xml");
 
-		new MainApp() {
+        String[] profiles = new String[1];
+        try {
+            String fileName = "config/devices.properties";
+            URL url = ClassLoader.getSystemResource(fileName);
+            File f = (url == null) ?  new File(fileName) : new File(url.toURI());
+            Properties properties = new Properties();
+            properties.load(new BufferedReader(new FileReader(f)));
 
-			protected void checkApplicationEnvironnement() {
-			};
+            String key = "plcSecure.behavior";
+            profiles[0] = key + "." + (String) properties.get(key);
 
-			protected String getApplicationVersion() {
-				InputStream versionFile = null;
-				try {
-					versionFile = ClassLoader.getSystemResourceAsStream("version");
-					return IOUtils.toString(versionFile);
-				} catch (Exception e) {
-					return "UNKNOWN_VERSION";
-				} finally {
-					if (versionFile != null) {
-						try {
-							versionFile.close();
-						} catch (Exception e2) {
-						}
-					}
-				}
-			};
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
 
-		}.loadApplicationAndStart(new LoaderConfig(config, "STANDARD\nSAS", new Runnable() {
-			@Override
-			public void run() {
-				//
-				// MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-				// try {
-				// ObjectName name = new ObjectName("com.sicpa.standard.sasscl.monitoring.mbean.sasscl:type=SassclApp");
-				// mbs.registerMBean(BeanProvider.getBean(BeansName.STATS_MBEAN), name);
-				//
-				// name = new ObjectName("com.sicpa.standard.sasscl.monitoring.mbean.sasscl:type=SassclRemoteControl");
-				// mbs.registerMBean(BeanProvider.getBean(BeansName.REMOTE_CONTROL_MBEAN), name);
-				//
-				// } catch (Exception e) {
-				// logger.error("", e);
-				// }
-			}
-		}));
+        new MainApp() {
+            protected void checkApplicationEnvironnement() {
+            };
 
-	}
+            protected String getApplicationVersion() {
+                InputStream versionFile = null;
+                try {
+                    versionFile = ClassLoader.getSystemResourceAsStream("version");
+                    return IOUtils.toString(versionFile);
+                } catch (Exception e) {
+                    // logger.error("", e);
+                    return "UNKNOWN_VERSION";
+                } finally {
+                    if (versionFile != null) {
+                        try {
+                            versionFile.close();
+                        } catch (Exception e2) {
+                        }
+                    }
+                }
+            }
+        }.loadApplicationAndStart(new LoaderConfig(config, "STANDARD\nSAS", new Runnable() {
+            @Override
+            public void run() {
+
+                // MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+                // try {
+                // ObjectName name = new ObjectName("com.sicpa.standard.sasscl.monitoring.mbean.sasscl:type=SassclApp");
+                // mbs.registerMBean(BeanProvider.getBean(BeansName.STATS_MBEAN), name);
+                //
+                // name = new ObjectName("com.sicpa.standard.sasscl.monitoring.mbean.sasscl:type=SassclRemoteControl");
+                // mbs.registerMBean(BeanProvider.getBean(BeansName.REMOTE_CONTROL_MBEAN), name);
+                //
+                // } catch (Exception e) {
+                // logger.error("", e);
+                // }
+
+                // add here customisation
+
+            }
+        }), (String[]) profiles);
+    }
 }
