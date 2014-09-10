37a2023697932800b4c87bb7c56ea3a3e17d36f0
lclaudon <lclaudon@20409334-78a3-4d4d-9d24-00d80d4599ef>
2014-09-10 11:24:59 +0000

    Load leibinger properties at spring initialization time into a bean
    git-svn-id: https://psdsvn.sicpa-net.ads/standard-framework/standard-sas-scl-modules/standard-sas-scl-parent/trunk@47416 20409334-78a3-4d4d-9d24-00d80d4599ef

--- standard-sas-scl-spring-scl/src/main/java/com/sicpa/standard/sasscl/ioc/SpringConfigSCL.java
+++ standard-sas-scl-spring-scl/src/main/java/com/sicpa/standard/sasscl/ioc/SpringConfigSCL.java
@@ -7,6 +7,7 @@ public class SpringConfigSCL extends SpringConfig {
 	public static final String PRINTER = PREFIX + "printer";
     public static final String PRINTER_DOMINO = PREFIX + "printer-domino";
     public static final String PRINTER_LEIBINGER = PREFIX + "printer-leibinger";
+    public static final String PRINTER_LEIBINGER_PROTOCOL = PREFIX + "leibingerProtocol";
     public static final String PRINTER_OI2JET = PREFIX + "printer-oi2jet";
     public static final String CODING = PREFIX + "coding";
 	public static final String POST_PACKAGE = PREFIX + "postPackage";
@@ -17,6 +18,8 @@ public class SpringConfigSCL extends SpringConfig {
 		this.config.put(PRINTER, "spring" + File.separator + "printer.xml");
         config.put(PRINTER_DOMINO, "spring/printer-domino.xml");
         config.put(PRINTER_LEIBINGER, "spring/printer-leibinger.xml");
+        config.put(PRINTER_LEIBINGER_PROTOCOL, "spring/leibingerProtocol.xml");
+
 //        Oi2Jet not immplemented yet
         config.put(PRINTER_OI2JET, "spring/printer-oi2jet.xml");
 
