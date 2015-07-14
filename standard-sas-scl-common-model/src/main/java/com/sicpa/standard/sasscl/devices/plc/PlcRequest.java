05337c828476ec90d23c743c07cafe0132a32d39
lclaudon <lclaudon@20409334-78a3-4d4d-9d24-00d80d4599ef>
2015-07-14 16:19:57 +0000

    Fix Tests
    git-svn-id: https://psdsvn.sicpa-net.ads/standard-framework/standard-sas-scl-modules/standard-sas-scl-parent/trunk@51698 20409334-78a3-4d4d-9d24-00d80d4599ef

--- standard-sas-scl-common-model/src/main/java/com/sicpa/standard/sasscl/devices/plc/PlcRequest.java
+++ standard-sas-scl-common-model/src/main/java/com/sicpa/standard/sasscl/devices/plc/PlcRequest.java
@@ -29,4 +29,20 @@ public class PlcRequest implements Serializable {
 		return this.description;
 	}
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		PlcRequest that = (PlcRequest) o;
+
+		if (description != null ? !description.equals(that.description) : that.description != null) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		return description != null ? description.hashCode() : 0;
+	}
 }
--- standard-sas-scl-device-plc/src/test/java/com/sicpa/standard/sasscl/devices/plc/PlcRequestTest.java
+++ standard-sas-scl-device-plc/src/test/java/com/sicpa/standard/sasscl/devices/plc/PlcRequestTest.java
@@ -1,16 +1,13 @@
 package com.sicpa.standard.sasscl.devices.plc;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class PlcRequestTest {
 
 	private static final String DESCRIPTION = "DESCRIPTION";
-	private static final int ID = 10;
 	private PlcRequest plcRequest;
 	private PlcRequest plcRequest2;
 
@@ -18,16 +15,6 @@ public class PlcRequestTest {
 	public void setUp() throws Exception {
 		plcRequest = new PlcRequest("");
 		plcRequest2 = new PlcRequest(DESCRIPTION);
-	}
-
-	@Test
-	public void testHashCodeByDefault() {
-		assertEquals(-1, plcRequest.hashCode());
-	}
-
-	@Test
-	public void testHashCode() {
-		assertEquals(ID, plcRequest2.hashCode());
 	}
 
 	@Test
