8cd3584dbe2b2b7a1378e813a27a63ade84a98d8
Victor Martinez <victor.martinezjurado@sicpa.com>
2015-08-18 18:42:42 +0200

    KESCL-14 remove unused imports

--- standard-sas-scl-common-model/src/main/java/com/sicpa/standard/sasscl/devices/remote/mapping/IProductionModeMapping.java
+++ standard-sas-scl-common-model/src/main/java/com/sicpa/standard/sasscl/devices/remote/mapping/IProductionModeMapping.java
@@ -2,12 +2,10 @@ package com.sicpa.standard.sasscl.devices.remote.mapping;
 
 import com.sicpa.standard.sasscl.model.ProductionMode;
 
-import java.util.List;
-
 public interface IProductionModeMapping {
 
-	ProductionMode getProductionModeFromRemoteId(int remoteid);
+    ProductionMode getProductionModeFromRemoteId(int remoteid);
 
-	void add(ProductionMode mode, int idOnRemote);
+    void add(ProductionMode mode, int idOnRemote);
 
 }
