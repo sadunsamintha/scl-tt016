9b1988e463c69d57ece6d65547d6834a4598f49b
Victor Martinez <victor.martinez@sicpa.com>
2015-12-01 15:34:36 +0100

    KESCL-8 add authenticator mode + some tests

---
+++ standard-sas-scl-common-model/src/test/java/com/sicpa/standard/sasscl/filter/DefaultCodeTypeFilterFactoryTest.java
@@ -0,0 +1,31 @@
+package com.sicpa.standard.sasscl.filter;
+
+
+import com.sicpa.standard.sasscl.model.CodeType;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class DefaultCodeTypeFilterFactoryTest {
+
+    @Test
+    public void defaultFilter() {
+
+        DefaultCodeTypeFilterFactory factory = new DefaultCodeTypeFilterFactory();
+
+        List<CodeType> codeTypes = new ArrayList<>();
+        codeTypes.add(new CodeType(1L));
+        codeTypes.add(new CodeType(2L));
+
+
+        Set<CodeType> codeTypesFilter = codeTypes.stream()
+                .filter(factory.getFilter()).collect(Collectors.toSet());
+
+        Assert.assertEquals(codeTypes.size(), codeTypesFilter.size());
+
+    }
+}
