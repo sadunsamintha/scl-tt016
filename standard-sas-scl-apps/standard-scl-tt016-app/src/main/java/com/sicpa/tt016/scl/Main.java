82684ee10fbca7bf7ffc8d38d3851f59ce9afb7e
Victor Martinez <victor.martinez@sicpa.com>
2016-03-30 16:02:20 +0200

    create  Morrocco profile skeleton within SCL Core Solution
    see STDSASSCL-1011

---
+++ standard-sas-scl-apps/standard-scl-tt016-app/src/main/java/com/sicpa/tt016/scl/Main.java
@@ -0,0 +1,11 @@
+package com.sicpa.tt016.scl;
+
+import com.sicpa.standard.sasscl.MainAppWithProfile;
+
+public class Main {
+
+	public static void main(final String[] args) {
+		//TT016Bootstrap.
+		new MainAppWithProfile().selectProfile();
+	}
+}
