44dd3e2d629b64f09d7aaab705e3c31bfef85653
unknown <dielsch@CHL0110616.sicpa-net.ads>
2016-04-01 14:18:07 +0200

    STDSASSCL-1072

--- standard-sas-scl-device-remoteServer/src/main/java/com/sicpa/standard/sasscl/devices/remote/simulator/RemoteServerSimulator.java
+++ standard-sas-scl-device-remoteServer/src/main/java/com/sicpa/standard/sasscl/devices/remote/simulator/RemoteServerSimulator.java
@@ -494,14 +494,4 @@ public class RemoteServerSimulator extends AbstractRemoteServer implements ISimu
 		return simulatorModel;
 	}
 
-	@Override
-	public void addToActivatedPackager(ProductStatus status) {
-
-	}
-
-	@Override
-	public void addToCounterPackager(ProductStatus status) {
-
-	}
-
 }
