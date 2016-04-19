c3da8593569ecb37932637faba14f15a26346cd4
De Almeida, Celso <celso.dealmeida@sicpa.com>
2016-04-19 15:12:32 +0200

    Changes based on team's review

--- standard-sas-scl-common-model/src/main/java/com/sicpa/standard/sasscl/utils/StringUtils.java
+++ standard-sas-scl-common-model/src/main/java/com/sicpa/standard/sasscl/utils/StringUtils.java
@@ -2,17 +2,19 @@ package com.sicpa.standard.sasscl.utils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.Arrays.*;
 
 public class StringUtils {
 
 	public static List<Long> commaDelimitedListToList(String values) {
 		if (values == null || values.isEmpty()) {
-			return new ArrayList<>();
+			return Collections.emptyList();
 		}
 
-		return Arrays.asList(values.split(",")).stream().mapToLong(Long::parseLong).boxed().collect(
-				Collectors.toList());
+		return asList(values.split(",")).stream().mapToLong(Long::parseLong).boxed().collect(Collectors.toList());
 	}
 }
