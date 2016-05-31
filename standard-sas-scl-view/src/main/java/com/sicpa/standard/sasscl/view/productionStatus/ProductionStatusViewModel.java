57eb187c5ff73b0b85718386740050f80a6a5111
Victor Martinez <victor.martinez@sicpa.com>
2016-05-31 16:10:54 +0200

    initial implementation of production status indicator
    see STDSASSCL-1085

---
+++ standard-sas-scl-view/src/main/java/com/sicpa/standard/sasscl/view/productionStatus/ProductionStatusViewModel.java
@@ -0,0 +1,7 @@
+package com.sicpa.standard.sasscl.view.productionStatus;
+
+
+import com.sicpa.standard.client.common.view.mvc.AbstractObservableModel;
+
+public class ProductionStatusViewModel extends AbstractObservableModel {
+}
