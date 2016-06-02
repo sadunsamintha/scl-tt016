25e5b44cf068cf6bf7fafb88dd1d72a823ecf8e0
unknown <dielsch@CHL0110616.sicpa-net.ads>
2016-06-02 08:54:53 +0200

    bis integration preparation

---
+++ standard-sas-scl-common-model/src/main/java/com/sicpa/standard/sasscl/skureader/ISkuFinder.java
@@ -0,0 +1,13 @@
+package com.sicpa.standard.sasscl.skureader;
+
+import java.util.Optional;
+
+import com.sicpa.standard.sasscl.model.SKU;
+
+public interface ISkuFinder {
+	
+	Optional<SKU> getSkuFromBarcode(String barcode);
+	
+	Optional<SKU> getSkuFromId(String id);
+
+}
