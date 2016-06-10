183afabf92477074d3ae61d5511e43da59e806d2
unknown <dielsch@CHL0110616.sicpa-net.ads>
2016-06-10 11:09:29 +0200

    fix view

--- standard-sas-scl-view/src/main/java/com/sicpa/standard/sasscl/view/MainFrame.java
+++ standard-sas-scl-view/src/main/java/com/sicpa/standard/sasscl/view/MainFrame.java
@@ -104,7 +104,7 @@ public class MainFrame extends AbstractMachineFrame {
 		SwingUtilities.invokeLater(() -> userChanged());
 	}
 
-	private void userChanged() {
+	protected void userChanged() {
 
 		resetAndRebuildAccessiblePanel();
 
