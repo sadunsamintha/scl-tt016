6fa0668730fc11dd5a4433449824e64bf761714e
unknown <DIelsch@CHL0110616.sicpa-net.ads>
2016-06-17 14:45:42 +0200

    STDSASSCL-1178

---
+++ standard-sas-scl-business-production/src/main/java/com/sicpa/standard/sasscl/business/production/impl/ProductionWithUnknownSkuHandling.java
@@ -0,0 +1,116 @@
+package com.sicpa.standard.sasscl.business.production.impl;
+
+import static com.sicpa.standard.client.common.utils.TaskExecutor.schedule;
+import static java.util.Collections.synchronizedList;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ScheduledFuture;
+
+import com.google.common.eventbus.Subscribe;
+import com.sicpa.standard.sasscl.business.activation.NewProductEvent;
+import com.sicpa.standard.sasscl.controller.flow.ApplicationFlowState;
+import com.sicpa.standard.sasscl.controller.flow.ApplicationFlowStateChangedEvent;
+import com.sicpa.standard.sasscl.model.Product;
+import com.sicpa.standard.sasscl.model.SKU;
+import com.sicpa.standard.sasscl.provider.impl.UnknownSkuProvider;
+
+public class ProductionWithUnknownSkuHandling extends Production {
+
+	private UnknownSkuProvider unknownSkuProvider;
+
+	private int unknownsBufferSize;
+	private int stillUnknownCheckDelaySec;
+
+	private final List<Product> unknownsBuffer = synchronizedList(new LinkedList<>());
+	private ScheduledFuture<?> stillUnknownTaskCheck;
+
+	@Override
+	public void notifyNewProduct(NewProductEvent evt) {
+		synchronized (unknownsBuffer) {
+			Product product = evt.getProduct();
+
+			if (isUnknown(product)) {
+				handleUnknownProduct(product);
+			} else {
+				handleKnownProduct(product);
+			}
+		}
+	}
+
+	private void handleUnknownProduct(Product product) {
+		unknownsBuffer.add(product);
+		if (isTooManyUnknown()) {
+			movePreviousUnknownToProductionBuffer();
+		} else {
+			if (stillUnknownTaskCheck != null) {
+				stillUnknownTaskCheck = schedule(() -> stillUnknownCheck(), stillUnknownCheckDelaySec, SECONDS, "");
+			}
+		}
+	}
+
+	private void stillUnknownCheck() {
+		synchronized (unknownsBuffer) {
+			movePreviousUnknownToProductionBuffer();
+			stillUnknownTaskCheck = null;
+		}
+	}
+
+	private void handleKnownProduct(Product product) {
+		resetStillUnknownTask();
+		if (!unknownsBuffer.isEmpty()) {
+			setSkuToPreviousUnknown(product.getSku());
+			movePreviousUnknownToProductionBuffer();
+		}
+		moveToProductionBuffer(product);
+	}
+
+	private void resetStillUnknownTask() {
+		if (stillUnknownTaskCheck != null) {
+			stillUnknownTaskCheck.cancel(false);
+			stillUnknownTaskCheck = null;
+		}
+	}
+
+	private void moveToProductionBuffer(Product product) {
+		super.notifyNewProduct(new NewProductEvent(product));
+	}
+
+	private boolean isTooManyUnknown() {
+		return unknownsBuffer.size() > unknownsBufferSize;
+	}
+
+	@Subscribe
+	public void handleApplicationStateChange(ApplicationFlowStateChangedEvent evt) {
+		if (evt.getCurrentState().equals(ApplicationFlowState.STT_SELECT_WITH_PREVIOUS)) {
+			synchronized (unknownsBuffer) {
+				movePreviousUnknownToProductionBuffer();
+			}
+		}
+	}
+
+	private boolean isUnknown(Product p) {
+		return unknownSkuProvider.get().equals(p.getSku());
+	}
+
+	private void setSkuToPreviousUnknown(SKU sku) {
+		unknownsBuffer.forEach(p -> p.setSku(sku));
+	}
+
+	private void movePreviousUnknownToProductionBuffer() {
+		unknownsBuffer.forEach(p -> moveToProductionBuffer(p));
+	}
+
+	public void setUnknownSkuProvider(UnknownSkuProvider unknownSkuProvider) {
+		this.unknownSkuProvider = unknownSkuProvider;
+	}
+
+	public void setUnknownsBufferSize(int unknownsBufferSize) {
+		this.unknownsBufferSize = unknownsBufferSize;
+	}
+
+	public void setStillUnknownCheckDelaySec(int stillUnknownCheckDelaySec) {
+		this.stillUnknownCheckDelaySec = stillUnknownCheckDelaySec;
+	}
+}
