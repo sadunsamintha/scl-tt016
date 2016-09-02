4beeb5a15b2ab6dd0e884206d7ccc16976c6914c
Victor Martinez <victor.martinez@sicpa.com>
2016-09-02 15:45:55 +0200

    add code review suggestion, replace loading manual global.properties by using the bean "allProperties"

--- standard-sas-scl-device-printer/src/main/java/com/sicpa/standard/sasscl/devices/printer/security/LeibingerPrinterUserLevelListener.java
+++ standard-sas-scl-device-printer/src/main/java/com/sicpa/standard/sasscl/devices/printer/security/LeibingerPrinterUserLevelListener.java
@@ -6,20 +6,16 @@ import com.sicpa.standard.client.common.security.ILoginListener;
 import com.sicpa.standard.client.common.security.SecurityService;
 import com.sicpa.standard.sasscl.devices.printer.util.LeibingerUtils;
 import com.sicpa.standard.sasscl.event.ChangePrinterUserLevelEvent;
-import com.sicpa.standard.sasscl.utils.ConfigUtilEx;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
 
 public class LeibingerPrinterUserLevelListener {
 
     private static final Logger logger = LoggerFactory.getLogger(LeibingerPrinterUserLevelListener.class);
 
+    private Properties allProperties;
 
 
     public void initLoginListenerLogger() {
@@ -46,11 +42,13 @@ public class LeibingerPrinterUserLevelListener {
 
     public int getPrinterUserLeveLoggedIn() {
         final String userLoggedIn = SecurityService.getCurrentUser().getLogin().toLowerCase();
-        final int printerUserLevel = LeibingerUtils.getPrinterUserLevelBySclUserName(userLoggedIn);
+        final int printerUserLevel = LeibingerUtils.getPrinterUserLevelBySclUserName(allProperties, userLoggedIn);
         logger.debug("The  printer user level of the current user loggedIn is {}", printerUserLevel);
         return printerUserLevel;
     }
 
-
+    public void setAllProperties(Properties allProperties) {
+        this.allProperties = allProperties;
+    }
 
 }
