204fbc6bd70c05e357768ad49fdbbc88f88969ab
Victor Martinez <victor.martinez@sicpa.com>
2016-09-02 15:56:44 +0200

    add code review suggestion, replace loading manual global.properties by using the bean "allProperties"

--- standard-sas-scl-device-printer/src/main/java/com/sicpa/standard/sasscl/devices/printer/util/LeibingerUtils.java
+++ standard-sas-scl-device-printer/src/main/java/com/sicpa/standard/sasscl/devices/printer/util/LeibingerUtils.java
@@ -1,23 +1,12 @@
 package com.sicpa.standard.sasscl.devices.printer.util;
 
 
-import com.sicpa.standard.sasscl.utils.ConfigUtilEx;
 import org.apache.commons.lang.Validate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
-
-import static com.sicpa.standard.client.common.utils.PropertiesUtils.extractAndDo;
-import static java.lang.Boolean.parseBoolean;
 
 public class LeibingerUtils {
 
-    private static final Logger logger = LoggerFactory.getLogger(LeibingerUtils.class);
 
     private static final String LEIBINGER_USER_LEVEL_KEY = "user.leibinger.level.%s";
 
