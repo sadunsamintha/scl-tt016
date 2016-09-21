f7f2c2c6fd61285367a6b026afea376aa5516a5f
eath Taylor <heath.taylor@sicpa.com>
2016-09-21 08:53:29 +0200

    Fixed log to use parameters refactored validate method

--- standard-sas-scl-controller-process/src/main/java/com/sicpa/standard/sasscl/controller/productionconfig/validator/ProductionParametersValidator.java
+++ standard-sas-scl-controller-process/src/main/java/com/sicpa/standard/sasscl/controller/productionconfig/validator/ProductionParametersValidator.java
@@ -1,7 +1,6 @@
 package com.sicpa.standard.sasscl.controller.productionconfig.validator;
 
 
-
 import com.sicpa.standard.client.common.eventbus.service.EventBusService;
 import com.sicpa.standard.sasscl.controller.productionconfig.IProductionConfig;
 import com.sicpa.standard.sasscl.controller.productionconfig.loader.IProductionConfigLoader;
@@ -19,16 +18,13 @@ public class ProductionParametersValidator {
     protected IProductionConfigLoader loader;
 
     public boolean validate(ProductionParameters productionParameters){
-        if (productionParameters !=null) {
-            return checkConfigurationExists(productionParameters.getProductionMode());
-        }
-        return false;
+        return productionParameters == null ? false : checkConfigurationExists(productionParameters.getProductionMode());
     }
 
     private boolean checkConfigurationExists(ProductionMode productionMode) {
         IProductionConfig config = loader.get(productionMode);
         if (config == null) {
-            logger.info("configuration_error,production_mode=" + productionMode + "reason=Configuration does not exist for this production mode");
+            logger.info("configuration_error,production_mode= {} reason=Configuration does not exist for this production mode",productionMode.getDescription());
             WarningViewEvent wve = new  WarningViewEvent("production.mode.config.does.not.exist",false, new Object[]{productionMode.getDescription()});
             EventBusService.post(wve);
             return false;
