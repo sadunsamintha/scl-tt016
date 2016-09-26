1a81a2398231ef363ead7bd7cfd17e8e33e8a125
Vieira <WVieira@sicpa-net.ads>
2016-09-26 13:55:31 -0300

    fix pull request

--- standard-sas-scl-apps/standard-scl-tt065-app/src/main/java/com/sicpa/tt065/scl/TT065Bootstrap.java
+++ standard-sas-scl-apps/standard-scl-tt065-app/src/main/java/com/sicpa/tt065/scl/TT065Bootstrap.java
@@ -3,6 +3,7 @@ package com.sicpa.tt065.scl;
 import com.sicpa.standard.sasscl.Bootstrap;
 import com.sicpa.standard.sasscl.messages.ActionMessageType;
 import com.sicpa.standard.sasscl.messages.MessageEventKey;
+
 import static com.sicpa.standard.sasscl.custoBuilder.CustoBuilder.setMessageType;
 
 /**
@@ -14,10 +15,5 @@ public class TT065Bootstrap extends Bootstrap {
     public void executeSpringInitTasks() {
         super.executeSpringInitTasks();
 
-        noStopIfDmxDetectedInExport();
-    }
-
-    private void noStopIfDmxDetectedInExport() {
-        setMessageType(MessageEventKey.Activation.EXCEPTION_CODE_IN_EXPORT, ActionMessageType.WARNING);
     }
 }
