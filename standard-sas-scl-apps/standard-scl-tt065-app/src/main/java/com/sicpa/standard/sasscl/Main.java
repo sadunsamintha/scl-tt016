f21947538574f76004b43f98056a5035fcaa9e82
Jimenez <MJimenez@sicpa-net.ads>
2016-11-30 11:31:51 -0200

    SIMAR-130 property file.encoding UTF-8

--- standard-sas-scl-apps/standard-scl-tt065-app/src/main/java/com/sicpa/standard/sasscl/Main.java
+++ standard-sas-scl-apps/standard-scl-tt065-app/src/main/java/com/sicpa/standard/sasscl/Main.java
@@ -5,6 +5,7 @@ package com.sicpa.standard.sasscl;
  */
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("file.encoding", "UTF-8");
         new MainAppWithProfile().selectProfile();
     }
 }
