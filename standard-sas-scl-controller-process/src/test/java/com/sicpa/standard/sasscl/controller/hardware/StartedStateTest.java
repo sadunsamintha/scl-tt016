42ffd80b7b4c6db1a6d67ec69c4b2272268bd322
De Almeida, Celso <celso.dealmeida@sicpa.com>
2016-12-20 17:21:15 +0100

    Fix StartedState test which was failing and added new test

--- standard-sas-scl-controller-process/src/test/java/com/sicpa/standard/sasscl/controller/hardware/StartedStateTest.java
+++ standard-sas-scl-controller-process/src/test/java/com/sicpa/standard/sasscl/controller/hardware/StartedStateTest.java
@@ -1,22 +1,23 @@
 package com.sicpa.standard.sasscl.controller.hardware;
 
-import java.util.Arrays;
-import java.util.List;
-
-import com.sicpa.standard.sasscl.devices.plc.PlcAdaptorException;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mockito;
-
 import com.google.common.eventbus.Subscribe;
 import com.sicpa.standard.client.common.eventbus.service.EventBusService;
+import com.sicpa.standard.client.common.messages.MessageEvent;
 import com.sicpa.standard.sasscl.controller.hardware.state.impl.StartedState;
 import com.sicpa.standard.sasscl.devices.DeviceStatus;
 import com.sicpa.standard.sasscl.devices.DeviceStatusEvent;
 import com.sicpa.standard.sasscl.devices.IStartableDevice;
 import com.sicpa.standard.sasscl.devices.plc.IPlcAdaptor;
+import com.sicpa.standard.sasscl.devices.plc.PlcAdaptorException;
+import com.sicpa.standard.sasscl.messages.MessageEventKey;
 import com.sicpa.standard.sasscl.test.utils.TestHelper;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class StartedStateTest {
 
@@ -75,10 +76,35 @@ public class StartedStateTest {
 	}
 
 	@Test
-	public void testDeviceStatusChanged() {
-		Mockito.when(startableDevice1.getName()).thenReturn("device1");
+	public void testBlockProductionDeviceDisconnect() {
+		setStartableDevice(startableDevice1, true, DeviceStatus.DISCONNECTED);
+		setStartableDevice(startableDevice2, true, DeviceStatus.CONNECTED);
+
 		startedState.deviceStatusChanged(new DeviceStatusEvent(DeviceStatus.DISCONNECTED, startableDevice1));
 		TestHelper.runAllTasks();
 		Mockito.verify(conveyorController).setCurrentState(stoppingState);
+	}
+
+	@Test
+	public void testNonBlockProductionDeviceDisconnect() {
+		EventBusService.register(new Object() {
+			@Subscribe
+			public void handleEvent(MessageEvent evt) {
+				Assert.assertEquals(MessageEventKey.DevicesController.DEVICE_DISCONNECT_WARNING, evt.getKey());
+			}
+		});
+
+		setStartableDevice(startableDevice1, false, DeviceStatus.DISCONNECTED);
+		setStartableDevice(startableDevice2, true, DeviceStatus.CONNECTED);
+		Mockito.when(plc.getStatus()).thenReturn(DeviceStatus.CONNECTED);
+
+		startedState.deviceStatusChanged(new DeviceStatusEvent(DeviceStatus.DISCONNECTED, startableDevice1));
+		TestHelper.runAllTasks();
+	}
+
+	private void setStartableDevice(IStartableDevice startableDevice, boolean blockProduction,
+									DeviceStatus deviceStatus) {
+		Mockito.when(startableDevice.getStatus()).thenReturn(deviceStatus);
+		Mockito.when(startableDevice.isBlockProductionStart()).thenReturn(blockProduction);
 	}
 }
