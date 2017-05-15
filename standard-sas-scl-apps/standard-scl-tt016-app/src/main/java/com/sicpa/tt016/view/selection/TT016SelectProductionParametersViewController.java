82bc80c945cef3af6f673a8a25dd49b3cb6216cb
De Almeida, Celso <celso.dealmeida@sicpa.com>
2017-05-15 18:11:17 +0200

    Logging total number of products of SKU

---
+++ standard-sas-scl-apps/standard-scl-tt016-app/src/main/java/com/sicpa/tt016/view/selection/TT016SelectProductionParametersViewController.java
@@ -0,0 +1,34 @@
+package com.sicpa.tt016.view.selection;
+
+import com.sicpa.standard.sasscl.common.log.OperatorLogger;
+import com.sicpa.standard.sasscl.model.ProductionParameters;
+import com.sicpa.standard.sasscl.view.ScreensFlowTriggers;
+import com.sicpa.standard.sasscl.view.selection.select.SelectProductionParametersViewController;
+import com.sicpa.tt016.devices.plc.PlcPersistentProductCounterManager;
+
+public class TT016SelectProductionParametersViewController extends SelectProductionParametersViewController {
+
+    private PlcPersistentProductCounterManager plcPersistentProductCounterManager;
+
+    @Override
+    public void productionParametersSelected(ProductionParameters pp) {
+        mainFrameController.setSku(pp.getSku());
+        mainFrameController.setProductionMode(pp.getProductionMode());
+        mainFrameController.setBarcode(pp.getBarcode());
+
+        OperatorLogger.log("Product Mode: {}", pp.getProductionMode().getDescription());
+        if (pp.getSku() != null) {
+            OperatorLogger.log("Product Param: {}", pp.getSku().getDescription());
+        }
+
+        mainFrameController.productionParametersChanged();
+
+        screensFlow.moveToNext(ScreensFlowTriggers.PRODUCTION_PARAMETER_SELECTED);
+
+        plcPersistentProductCounterManager.execute();
+    }
+
+    public void setPlcPersistentProductCounterManager(PlcPersistentProductCounterManager plcPersistentProductCounterManager) {
+        this.plcPersistentProductCounterManager = plcPersistentProductCounterManager;
+    }
+}
