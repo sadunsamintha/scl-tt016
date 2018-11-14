ba3e9e2fe8820c12c15a4899ca4b7e566b535387
Martinez Jurado, Victor <victor.martinez@sicpa.com>
2018-11-14 12:16:11 +0100

    add missing Encoders to provide composed codes with code + HRC when master in simulation

---
+++ standard-sas-scl-apps/standard-scl-tt077-app/src/main/java/com/sicpa/tt077/remote/stdCrypto/TT077CryptoEncoderWrapperSimulator.java
@@ -0,0 +1,36 @@
+package com.sicpa.tt077.remote.stdCrypto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sicpa.standard.sasscl.devices.remote.stdCrypto.ICryptoFieldsConfig;
+import com.sicpa.standard.sasscl.devices.remote.stdCrypto.StdCryptoEncoderWrapperSimulator;
+import com.sicpa.standard.sasscl.sicpadata.CryptographyException;
+import com.sicpa.standard.sicpadata.api.business.IBSicpadataGenerator;
+
+import static com.sicpa.tt077.remote.impl.sicpadata.TT077SicpaDataGeneratorWrapper.BLOCK_SEPARATOR;
+
+
+public class TT077CryptoEncoderWrapperSimulator extends StdCryptoEncoderWrapperSimulator {
+    private static final long serialVersionUID = 1L;
+
+    private long sequence = 0;
+
+    public TT077CryptoEncoderWrapperSimulator(long batchid, int id, IBSicpadataGenerator encoder, int year, long subsystemId, long maxCode, ICryptoFieldsConfig cryptoFieldsConfig, int codeTypeId) {
+        super(batchid, id, encoder, year, subsystemId, maxCode, cryptoFieldsConfig, codeTypeId);
+    }
+
+    @Override
+    public synchronized List<String> getEncryptedCodes(long numberCodes) throws CryptographyException {
+        List<String> res = super.getEncryptedCodes(numberCodes);
+
+        List<String> codes = new ArrayList<String>();
+
+        for(String s : res) {
+            codes.add(s + BLOCK_SEPARATOR + String.format("%09d", String.valueOf(sequence++)));
+        }
+
+        remainingCode -= res.size();
+        return codes;
+    }
+}
