01cc9ae3f09630bfef8e553e25452c45a3642de8
Chowdury, Raihanur Rahman <RaihanurRahman.Chowdury@sicpa.com>
2019-05-07 10:17:27 +0800

    Added condition to reset time out delay to 45 seconds if conifgured lower than that.

--- standard-sas-scl-controller-process/src/main/java/com/sicpa/standard/sasscl/controller/flow/statemachine/executor/ExecutorStarting.java
+++ standard-sas-scl-controller-process/src/main/java/com/sicpa/standard/sasscl/controller/flow/statemachine/executor/ExecutorStarting.java
@@ -5,14 +5,19 @@ import com.sicpa.standard.client.common.controller.predicate.start.NoStartReason
 import com.sicpa.standard.client.common.eventbus.service.EventBusService;
 import com.sicpa.standard.client.common.messages.MessageEvent;
 import com.sicpa.standard.client.common.statemachine.IStateAction;
+import com.sicpa.standard.client.common.utils.PropertiesUtils;
 import com.sicpa.standard.client.common.utils.TaskExecutor;
 import com.sicpa.standard.sasscl.common.utils.Timeout;
 import com.sicpa.standard.sasscl.controller.hardware.IHardwareController;
 import com.sicpa.standard.sasscl.messages.MessageEventKey;
 import com.sicpa.standard.sasscl.provider.impl.ProductionBatchProvider;
+import com.sicpa.standard.sasscl.utils.ConfigUtilEx;
+import org.apache.commons.configuration.ConfigurationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.File;
 import java.util.TimerTask;
 
 public class ExecutorStarting implements IStateAction {
@@ -26,6 +31,7 @@ public class ExecutorStarting implements IStateAction {
 
 	// to be able to stop the production if the start takes too long
 	private Timeout timeoutStart;
+	final int MINIMUM_TIME_OUT_DELAY_LIMIT = 45;
 
 	@Override
 	public void enter() {
@@ -40,7 +46,7 @@ public class ExecutorStarting implements IStateAction {
 		startTimeoutStart();
 		updateNextProductionBatchTime();
 		startHardware();
-        stopTimeoutStart();
+		stopTimeoutStart();
 	}
 
 	private void startHardware() {
@@ -60,7 +66,7 @@ public class ExecutorStarting implements IStateAction {
 	private boolean validatePossibleToStart() {
 		NoStartReason reasons = startValidators.isPossibleToStartProduction();
 
-        for (String r : reasons.getReasons()) {
+		for (String r : reasons.getReasons()) {
 			logger.error("failed to start:" + r);
 			EventBusService.post(new MessageEvent(r));
 		}
@@ -80,6 +86,9 @@ public class ExecutorStarting implements IStateAction {
 	}
 
 	private Timeout createStartTimeout() {
+		if (timeoutDelay < MINIMUM_TIME_OUT_DELAY_LIMIT * 1000) {
+			resetTimeoutDelayToDefault();
+		}
 		return new Timeout("Start timeout", new TimerTask() {
 			@Override
 			public void run() {
@@ -88,6 +97,17 @@ public class ExecutorStarting implements IStateAction {
 				EventBusService.post(new MessageEvent(MessageEventKey.FlowControl.START_TIMEOUT));
 			}
 		}, timeoutDelay);
+	}
+
+	private void resetTimeoutDelayToDefault() {
+		try {
+			File globalPropertiesFile = new ClassPathResource(ConfigUtilEx.GLOBAL_PROPERTIES_PATH).getFile();
+			PropertiesUtils.savePropertiesKeepOrderAndComment(globalPropertiesFile,
+					"startProduction.timeoutInSec", Long.toString(MINIMUM_TIME_OUT_DELAY_LIMIT));
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		setTimeoutDelay(MINIMUM_TIME_OUT_DELAY_LIMIT);
 	}
 
 	public void setStartValidators(IStartProductionValidator startValidators) {
