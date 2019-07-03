cfa4d9243846c2281a6662b0aae99e5c5cc290ea
Amarnath, Busa <Busa.Amarnath@sicpa.com>
2019-07-03 17:26:51 +0800

    Added Hrc to existing core simulator

---
+++ standard-sas-scl-device-remoteServer/src/main/java/com/sicpa/standard/sasscl/devices/remote/stdCrypto/StdHrdCryptoEncoderWrapperSimulator.java
@@ -0,0 +1,34 @@
+package com.sicpa.standard.sasscl.devices.remote.stdCrypto;
+
+import com.sicpa.standard.sasscl.sicpadata.CryptographyException;
+import com.sicpa.standard.sicpadata.api.business.IBSicpadataGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StdHrdCryptoEncoderWrapperSimulator extends StdCryptoEncoderWrapperSimulator {
+
+	public final static String BLOCK_SEPARATOR = ">-<" ;
+	
+	private static final long serialVersionUID = 1L;
+	
+    private long sequence = 1;
+
+	public StdHrdCryptoEncoderWrapperSimulator(long batchid, int id, IBSicpadataGenerator encoder, int year, long subsystemId, long maxCode, ICryptoFieldsConfig cryptoFieldsConfig, int codeTypeId) {
+        super(batchid, id, encoder, year, subsystemId, maxCode, cryptoFieldsConfig, codeTypeId);
+    }
+	
+	@Override
+    public synchronized List<String> getEncryptedCodes(long numberCodes) throws CryptographyException {
+        List<String> res = super.getEncryptedCodes(numberCodes);
+
+        List<String> codes = new ArrayList<String>();
+
+        for(String s : res) {
+        	codes.add(s + BLOCK_SEPARATOR + String.format("%09d", this.sequence++));
+        }
+
+        remainingCode -= res.size();
+        return codes;
+    }
+}
--- standard-sas-scl-spring-common/src/main/resources/spring/printer/printer-common.groovy
+++ standard-sas-scl-spring-common/src/main/resources/spring/printer/printer-common.groovy
@@ -9,6 +9,11 @@ beans{
 		blobPosition=props['blob.position']
 		blobType=props['blob.type']
 		blobUtils=ref('blobDetectionUtils')
+		text1Length=props['text1.length']
+		text1Position=props['text1.position']
+		text2Length=props['text2.length']
+		text2Position=props['text2.position']
+		hrdEnable=props['hrd.enable']
 	}
 	mappingExtendedCodeBehavior(MappingExtendedCodeBehavior){ defaultBehavior=ref('extendedCodeBehavior') }
 }
