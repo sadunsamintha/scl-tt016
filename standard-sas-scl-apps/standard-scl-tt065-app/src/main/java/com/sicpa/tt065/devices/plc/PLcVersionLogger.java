8bd61e65d0f36fd79f441edcb8e57ad73d0d8f2e
ASantana <ARTUR.SANTANA@CEPTIS.COM.BR>
2020-05-18 15:39:58 -0300

    SIMAR-5276 Correcting PLCVersionLogger to log the plc's version just once.

--- standard-sas-scl-apps/standard-scl-tt065-app/src/main/java/com/sicpa/tt065/devices/plc/PLcVersionLogger.java
+++ standard-sas-scl-apps/standard-scl-tt065-app/src/main/java/com/sicpa/tt065/devices/plc/PLcVersionLogger.java
@@ -6,34 +6,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PLcVersionLogger implements IPlcVersionLogger{
-
     private PlcProvider plcProvider;
-
-    //private IPlcAdaptor plc;
-    private static final Logger logger = LoggerFactory.getLogger(PLcVersionLogger.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(PLcVersionLogger.class);
 
     public void logPLCVersion() {
-
         if (plcProvider != null){
-
-            IPlcAdaptor plc = plcProvider.get();
-
+            final IPlcAdaptor plc = plcProvider.get();
             if (plc != null && plc.isConnected()) {
-
-                logger.info("PLC Version: " + plc.getPlcVersion());
+                plc.getPlcVersion();
             } else {
-
-                logger.debug("plc is null");
+                LOGGER.debug("plc is null");
             }
         } else {
-
-            logger.debug("plcProvider is null");
+            LOGGER.debug("plcProvider is null");
         }
-
     }
 
     public void setPlcProvider(PlcProvider plcProvider) {
         this.plcProvider = plcProvider;
     }
-
 }
