bd7287d7ca024af5d6e94a18b966577f99f8f410
Chowdury, Raihanur Rahman <RaihanurRahman.Chowdury@sicpa.com>
2020-06-29 04:07:17 +0200

    Merge pull request #470 in CC/sas-scl-std from feature/TTTH-70-scl-application-ui-in-thai-language to develop
    Squashed commit of the following:
    
    commit 4d731bc970ee7fdf112be244b1a4ebc3673eea79
    Author: Chowdury, Raihanur Rahman <RaihanurRahman.Chowdury@sicpa.com>
    Date:   Thu Jun 25 23:53:33 2020 +0800
    
        Implemented interface changes and language file for the gui to properly render with Thai Characters.

--- standard-sas-scl-common-model/src/main/java/com/sicpa/standard/sasscl/utils/StringUtils.java
+++ standard-sas-scl-common-model/src/main/java/com/sicpa/standard/sasscl/utils/StringUtils.java
@@ -1,5 +1,7 @@
 package com.sicpa.standard.sasscl.utils;
 
+import static java.nio.charset.StandardCharsets.ISO_8859_1;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 
@@ -14,5 +16,9 @@ public class StringUtils {
 		}
 
 		return asList(values.split(",")).stream().mapToLong(Long::parseLong).boxed().collect(toList());
+	}
+
+	public static String convertToUTF8(String str) {
+		return new String(str.getBytes(ISO_8859_1), UTF_8);
 	}
 }
--- standard-sas-scl-spring-common/src/main/resources/spring/custo/ttth/ttth.groovy
+++ standard-sas-scl-spring-common/src/main/resources/spring/custo/ttth/ttth.groovy
@@ -1,10 +1,20 @@
 import com.sicpa.ttth.scl.TTTHBootstrap
 
 beans {
-	addAlias('bootstrapAlias','bootstrap')
-	bootstrap(TTTHBootstrap){b->
-		b.parent=ref('bootstrapAlias')
-	}
+    def serverBehavior = props['remoteServer.behavior'].toUpperCase()
 
-	importBeans('spring/custo/ttth/ttth-printer-common.groovy')
+    addAlias('bootstrapAlias', 'bootstrap')
+    bootstrap(TTTHBootstrap) { b ->
+        b.parent = ref('bootstrapAlias')
+    }
+
+    //TODO: Replace with thailand server implementation.
+    if (serverBehavior == "STANDARD") {
+        importBeans('spring/server/server-core5.groovy')
+    }
+    if (serverBehavior == "SIMULATOR") {
+        importBeans('spring/server/server-simulator.groovy')
+    }
+
+    importBeans('spring/custo/ttth/ttth-printer-common.groovy')
 }
