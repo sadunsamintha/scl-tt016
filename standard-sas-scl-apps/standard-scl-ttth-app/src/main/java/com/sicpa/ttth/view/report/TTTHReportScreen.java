a7c5840cf3d2a6c8ac51dd98991a5bc88b2107b7
Chowdury, Raihanur Rahman <RaihanurRahman.Chowdury@sicpa.com>
2020-12-14 07:50:24 +0100

    Merge pull request #600 in CC/sas-scl-std from bugfix/TTTH-974-scl-not-having-buddist-calendar to develop
    Squashed commit of the following:
    
    commit 00efa3a1a9e96b11f54163dc69774f44e32cb538
    Author: Chowdury, Raihanur Rahman <RaihanurRahman.Chowdury@sicpa.com>
    Date:   Mon Dec 14 13:33:22 2020 +0800
    
        Handle Double call time set from JxDatePicker.
    
    commit 7966f3f04a2e38756a954c816463d1c8afe7d089
    Author: Chowdury, Raihanur Rahman <RaihanurRahman.Chowdury@sicpa.com>
    Date:   Mon Dec 14 12:22:27 2020 +0800
    
        Removed manual increment to buddhist calendar.

--- standard-sas-scl-apps/standard-scl-ttth-app/src/main/java/com/sicpa/ttth/view/report/TTTHReportScreen.java
+++ standard-sas-scl-apps/standard-scl-ttth-app/src/main/java/com/sicpa/ttth/view/report/TTTHReportScreen.java
@@ -1,24 +1,22 @@
 package com.sicpa.ttth.view.report;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
-import org.apache.commons.lang.time.DateUtils;
 import org.jdesktop.swingx.JXDatePicker;
 
-import com.sicpa.standard.sasscl.view.monitoring.IProductionStatisticsAggregator;
 import com.sicpa.standard.sasscl.view.report.IReportTable;
 import com.sicpa.standard.sasscl.view.report.ReportScreen;
 
-import static com.sicpa.ttth.scl.utils.TTTHCalendarUtils.TH_YEAR_DIFF;
-
 
 public class TTTHReportScreen extends ReportScreen {
-	
-	private static final long serialVersionUID = 1L;
+
+    private static final long serialVersionUID = 1L;
+    private static final long unixEpochInitTime = 1970;
+
+    private Date selectedDate;
 
     @Override
     public IReportTable getTable() {
@@ -30,43 +28,27 @@ public class TTTHReportScreen extends ReportScreen {
     }
 
     @Override
-    protected void getAndShowReport() {
-    	if (getDateFrom().getDate() == null || getDateTo().getDate() == null) {
-			return;
-		}
-    	
-        IProductionStatisticsAggregator aggre = productionStatisticsAggregatorFactory.getInstance();
-
-        Date from = DateUtils.round(convertBackToGeorgian(getDateFrom().getDate()), Calendar.DAY_OF_MONTH);
-        Date to = DateUtils.ceiling(convertBackToGeorgian(getDateTo().getDate()), Calendar.DAY_OF_MONTH);// go to next day
-        to = new Date(to.getTime() - 1);// remove 1 ms to come back
-
-        aggre.aggregate(getProductionStatistics(from, to), getSelectedPeriod(), getButtonGroupByProduct().isSelected(),
-            getButtonDailyDetailed().isSelected(), from, to);
-        getTable().setData(aggre.getMapData(), getButtonGroupByProduct().isSelected(),
-            getButtonDailyDetailed().isSelected());
-    }
-
-    @Override
     public JXDatePicker getDateFrom() {
         if (this.dateFrom == null) {
             this.dateFrom = new JXDatePicker();
-            DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            DateFormat dateFormat = new SimpleDateFormat("dd/MM");
             this.dateFrom.setFormats(dateFormat);
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(new Date());
-            calendar.add(Calendar.YEAR, TH_YEAR_DIFF);
-            this.dateFrom.setDate(calendar.getTime());
-            
-            this.dateFrom.addPropertyChangeListener("date", new PropertyChangeListener() {
-                @Override
-                public void propertyChange(PropertyChangeEvent evt) {
-                    Date selectedDate = dateFrom.getDate();
-                    
+            this.dateFrom.setDate(new Date());
+
+            this.dateFrom.addPropertyChangeListener("date", evt -> {
+                LocalDate localDate = dateFrom
+                    .getDate()
+                    .toInstant()
+                    .atZone(ZoneId.systemDefault()).toLocalDate();
+                //Handles the double call issue for JXDatePicker
+                if (localDate.getYear() != unixEpochInitTime) {
+                    selectedDate = dateFrom.getDate();
                     if (selectedDate != null) {
-                    	dateTo.getMonthView().setLowerBound(selectedDate);
+                        dateTo.getMonthView().setLowerBound(selectedDate);
                         dateTo.setDate(selectedDate);
                     }
+                } else {
+                    dateFrom.setDate(selectedDate);
                 }
             });
         }
@@ -77,22 +59,11 @@ public class TTTHReportScreen extends ReportScreen {
     public JXDatePicker getDateTo() {
         if (this.dateTo == null) {
             this.dateTo = new JXDatePicker();
-            DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            DateFormat dateFormat = new SimpleDateFormat("dd/MM");
             this.dateTo.setFormats(dateFormat);
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(new Date());
-            calendar.add(Calendar.YEAR, TH_YEAR_DIFF);
-            this.dateTo.setDate(calendar.getTime());
-            this.dateTo.getMonthView().setLowerBound(calendar.getTime());
+            this.dateTo.setDate(new Date());
+            this.dateTo.getMonthView().setLowerBound(new Date());
         }
         return this.dateTo;
     }
-
-    private Date convertBackToGeorgian(Date date) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        calendar.add(Calendar.YEAR, -TH_YEAR_DIFF);
-        return calendar.getTime();
-    }
-
 }
