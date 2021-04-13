172a0acdc9a8938ec908a11835daf27ce4f285f1
Muniandy, Shashinath <Shashinath.Muniandy@sicpa.com>
2021-04-13 09:56:30 +0200

    Merge pull request #678 in CC/sas-scl-std from bugfix/STDSASSCL-2688-fix-invalid-ink-detected-status to develop
    Squashed commit of the following:
    
    commit 13b725a4fed9d253827615a7b79f5ce6873d0153
    Author: Shashinath <shashinath.muniandy@sicpa.com>
    Date:   Mon Apr 12 15:01:57 2021 +0800
    
        [STDSASSCL-2673] Change INK_DETECTED in refeed mode to refeed

--- standard-sas-scl-business-activation/src/main/java/com/sicpa/standard/sasscl/business/activation/impl/activationBehavior/RefeedActivationBehavior.java
+++ standard-sas-scl-business-activation/src/main/java/com/sicpa/standard/sasscl/business/activation/impl/activationBehavior/RefeedActivationBehavior.java
@@ -1,17 +1,36 @@
 package com.sicpa.standard.sasscl.business.activation.impl.activationBehavior;
 
 import com.sicpa.standard.sasscl.business.activation.impl.activationBehavior.standard.StandardActivationBehavior;
+import com.sicpa.standard.sasscl.model.Code;
 import com.sicpa.standard.sasscl.model.DecodedCameraCode;
 import com.sicpa.standard.sasscl.model.Product;
 import com.sicpa.standard.sasscl.model.ProductStatus;
 
 public class RefeedActivationBehavior extends StandardActivationBehavior {
 
-	@Override
-	protected void checkProduct(DecodedCameraCode result, Product product) {
-		super.checkProduct(result, product);
-		if (ProductStatus.AUTHENTICATED.equals( product.getStatus()) ) {
-			product.setStatus(ProductStatus.REFEED);
-		}
-	}
+
+  private String productionBehaviorVar;
+
+  @Override
+  protected void checkProduct(DecodedCameraCode result, Product product) {
+    super.checkProduct(result, product);
+    if (ProductStatus.AUTHENTICATED.equals(product.getStatus())) {
+      product.setStatus(ProductStatus.REFEED);
+    }
+  }
+
+  @Override
+  public Product receiveCode(Code code, boolean valid) {
+
+    if ("PRODUCTIONCONFIG-SCL".equalsIgnoreCase(productionBehaviorVar) && null != code.getStringCode()
+        && ProductStatus.INK_DETECTED.getId() == Long.parseLong(code.getStringCode())) {
+      return super.receiveCode(code, true);
+    } else {
+      return super.receiveCode(code, valid);
+    }
+  }
+
+  public void setProductionBehaviorVar(String productionBehaviorVar) {
+    this.productionBehaviorVar = productionBehaviorVar;
+  }
 }
