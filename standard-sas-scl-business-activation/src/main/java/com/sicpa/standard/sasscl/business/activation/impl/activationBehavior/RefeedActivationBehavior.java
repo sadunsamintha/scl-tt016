d937818be9d3d17c712d74662f1b58a07bc84601
Muniandy, Shashinath <Shashinath.Muniandy@sicpa.com>
2021-05-04 05:30:13 +0200

    Merge pull request #686 in CC/sas-scl-std from bugfix/TT079-7882-fix-tt079-refeed-issues to develop
    Squashed commit of the following:
    
    commit a412e97ffbef465b547487e73ce29660646cb5f0
    Author: Shashinath <shashinath.muniandy@sicpa.com>
    Date:   Mon May 3 14:20:22 2021 +0800
    
        [TT079-7859] Fix INK_DETECTED and VALID code issues during refeed

--- standard-sas-scl-business-activation/src/main/java/com/sicpa/standard/sasscl/business/activation/impl/activationBehavior/RefeedActivationBehavior.java
+++ standard-sas-scl-business-activation/src/main/java/com/sicpa/standard/sasscl/business/activation/impl/activationBehavior/RefeedActivationBehavior.java
@@ -23,7 +23,7 @@ public class RefeedActivationBehavior extends StandardActivationBehavior {
   public Product receiveCode(Code code, boolean valid) {
 
     if ("PRODUCTIONCONFIG-SCL".equalsIgnoreCase(productionBehaviorVar) && null != code.getStringCode()
-        && ProductStatus.INK_DETECTED.getId() == Long.parseLong(code.getStringCode())) {
+        && String.valueOf(ProductStatus.INK_DETECTED.getId()).equals(code.getStringCode())) {
       return super.receiveCode(code, true);
     } else {
       return super.receiveCode(code, valid);
@@ -32,5 +32,23 @@ public class RefeedActivationBehavior extends StandardActivationBehavior {
 
   public void setProductionBehaviorVar(String productionBehaviorVar) {
     this.productionBehaviorVar = productionBehaviorVar;
+  }
+
+  protected DecodedCameraCode getDecodedCameraCode(Code code) {
+    if ("PRODUCTIONCONFIG-SCL".equalsIgnoreCase(productionBehaviorVar) && null != code.getStringCode()
+        && String.valueOf(ProductStatus.INK_DETECTED.getId()).equals(code.getStringCode())) {
+
+      // INK_DETECTED codes have "100" as their code so the SICPADATA could not be decoded from the code. So, we have to
+      // manually set the values for INK_DETECTED as AUTHENTICATED to ensure it will be counted as REFEED instead of INVALID
+      DecodedCameraCode res = new DecodedCameraCode();
+      res.setCodeType(productionParameters.getSku().getCodeType());
+      res.setBatchId(-1);
+      res.setAuthenticated(true);
+      res.setSequence(Long.parseLong(code.getStringCode()));
+
+      return res;
+    } else {
+      return super.getDecodedCameraCode(code);
+    }
   }
 }
