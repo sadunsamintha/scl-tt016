7b5c75b346281f41df8e1d4c0164326a7fad541c
Chowdury, Raihanur Rahman <RaihanurRahman.Chowdury@sicpa.com>
2021-05-21 05:39:11 +0200

    Merge pull request #694 in CC/sas-scl-std from feature/TTTH-2312-add-inhibit-function-for-70-and-100-for-ejection-table to develop
    Squashed commit of the following:
    
    commit 80e21ae00b4f0df049c33b01031160c4875162ee
    Author: Chowdury, Raihanur Rahman <RaihanurRahman.Chowdury@sicpa.com>
    Date:   Wed May 19 15:29:02 2021 +0800
    
        TTTH-2312-add-inhibit-function-for-70-and-100-for-ejection-table
    
        Added PLC vars for inhibit partial and full ejection table vars.
    
        Removed Eject confirmation pass.

--- standard-sas-scl-apps/standard-scl-ttth-app/src/main/java/com/sicpa/standard/sasscl/devices/plc/EjectorPlcEnums.java
+++ standard-sas-scl-apps/standard-scl-ttth-app/src/main/java/com/sicpa/standard/sasscl/devices/plc/EjectorPlcEnums.java
@@ -4,12 +4,13 @@ public enum EjectorPlcEnums {
 
 	PARAM_LINE_EJECT_CONFIRM_TYPE(".com.stLine[#x].stParameters.bEjectConfirmType", PlcUtils.PLC_TYPE.B),
 	PARAM_LINE_INHIBIT_EJECT_CONFIRM_EJECT(".com.stLine[#x].stParameters.bInhibitEjectConfirm_Eject", PlcUtils.PLC_TYPE.B),
-	PARAM_LINE_INHIBIT_EJECT_CONFIRM_PASS(".com.stLine[#x].stParameters.bInhibitEjectConfirm_Pass", PlcUtils.PLC_TYPE.B),
 	PARAM_LINE_EJECT_CONFIRM_DISTANCE(".com.stLine[#x].stParameters.nEjectConfirmDistance", PlcUtils.PLC_TYPE.I),
 	PARAM_LINE_EJECT_CONFIRM_LENGTH(".com.stLine[#x].stParameters.nEjectConfirmLength", PlcUtils.PLC_TYPE.I),
 	PARAM_LINE_EJECT_TABLE_FULL_MILLISECOND(".com.stLine[#x].stParameters.nEjectTableFullMillisecond", PlcUtils.PLC_TYPE.I),
 	PARAM_LINE_EJECT_TABLE_PARTIAL_FULL_MILLISECOND(".com.stLine[#x].stParameters.nEjectTablePartialFullMillisecond", PlcUtils.PLC_TYPE.I),
-	PARAM_LINE_INHIBIT_PRODUCTION_STOP_EJECTION(".com.stLine[#x].stParameters.bInhibitProductionStopEjection", PlcUtils.PLC_TYPE.B);
+	PARAM_LINE_INHIBIT_PRODUCTION_STOP_EJECTION(".com.stLine[#x].stParameters.bInhibitProductionStopEjection", PlcUtils.PLC_TYPE.B),
+	PARAM_LINE_INHIBIT_EJECT_TABLE_PARTIAL(".com.stLine[#x].stParameters.bInhibitEjectTablePartial", PlcUtils.PLC_TYPE.B),
+	PARAM_LINE_INHIBIT_EJECT_TABLE_FULL(".com.stLine[#x].stParameters.bInhibitEjectTableFull", PlcUtils.PLC_TYPE.B);
 
 	private String nameOnPlc;
 	private PlcUtils.PLC_TYPE plc_type;
