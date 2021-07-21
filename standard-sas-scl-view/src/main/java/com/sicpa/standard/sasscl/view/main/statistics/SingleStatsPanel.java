0a06f39985c0a9fb8f7c6e0f889f3574f69d4834
Regonda, Abilash <Abilash.Regonda@sicpa.com>
2021-07-21 05:29:12 +0200

    Merge pull request #712 in CC/sas-scl-std from bugfix/TT079-7895-percentage-figures-not-fully-shown-on-application-statistics to develop
    Squashed commit of the following:
    
    commit fecc4f549dbb277c3974fb7a3d4d4a6ff60982b1
    Author: aregonda <aregonda@10.1.144.1>
    Date:   Thu Jul 15 15:01:30 2021 +0800
    
        TT079-7895: Percentage figures not fully shown on application statistics

--- standard-sas-scl-view/src/main/java/com/sicpa/standard/sasscl/view/main/statistics/SingleStatsPanel.java
+++ standard-sas-scl-view/src/main/java/com/sicpa/standard/sasscl/view/main/statistics/SingleStatsPanel.java
@@ -35,7 +35,7 @@ public class SingleStatsPanel extends JPanel {
 		labelCount.setForeground(descriptor.getColor());
 
 		add(labelCount, "w 120!,left");
-		add(labelPercent, "w 65!, left,pushx");
+		add(labelPercent, "w 85!, left,pushx");
 	}
 
 	public void setCount(String count) {
